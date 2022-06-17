775cedcb4f130289e03aefa888cbf38ff3dbe367
Yan Krug Gerhardt <yan-ragna@hotmail.com>
2022-06-16 22:53:09 -0300

    modified style

--- lab03.py
+++ lab03.py
@@ -5,6 +5,7 @@ def titulo(msg):
   print('-'*100)
   print(f'{msg:^110}')
   print('-'*100)
+
   
 titulo('\033[31mH A N G M A N\033[m')
 
@@ -69,23 +70,29 @@ c2 = 0
 while True:
   chute = str(input('\nChute uma letra: '))
   if chute in sorteio:
-    print('\nAcertou\n')
+    print('\n\033[32mAcertou\033[m\n')
     letras_certas.append(chute)
     for letra in sorteio:
       if letra in letras_certas:
-        print(letra , end='')
+        print('\033[33m',letra,'\033[m' , end='')
       else:
         print(' _ ', end='')
     print()
     # Faltou descobrir como fazer a contagem das letras já descobertas e igualar elas com o tamanho da palavra. Problema: letras repetidas.
     if len(letras_certas) == len(sorteio):
-      print(f'\033[32mParabéns!! Você ganhou!\033[m\nA palavra era \033[32m{sorteio}\033[m.')
+      print(f'\033[32mParabéns!! Você ganhou!\033[m')
       break
   else: 
-    print('\n\033[31mLetra não está na palavra.\033[m')
+    print(f'\n\033[31mLetra {chute} não está na palavra.\033[m')
     letras_erradas.append(chute)
     print(forca.get(c1))
     c1 += 1
+    for letra in sorteio:
+      if letra in letras_certas:
+        print('\033[33m',letra,'\033[m' , end='')
+      else:
+        print(' _ ', end='')
+    print()
   if c1 == 8:
     print(f'\033[31mVocê perdeu.\033[m\nA palavra era \033[32m{sorteio}\033[m.')
     print('Mais sorte na próxima vez.')
