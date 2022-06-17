ce97f8eb4c58c779c415cac8710614047059ff9e
Yan Krug Gerhardt <yan-ragna@hotmail.com>
2022-06-17 09:52:56 -0300

    Finished project, uhuu

--- lab03.py
+++ lab03.py
@@ -68,20 +68,28 @@ print('_ '*len(sorteio))
 c1 = 2
 c2 = 0
 while True:
-  chute = str(input('\nChute uma letra: '))
+  while True:
+    chute = str(input('\nChute uma letra: ')).strip()[0]
+    if chute in letras_certas or chute in letras_certas:
+      print(f'Letra \033[31m{chute}\033[m já chutada, tente novamente.')
+    else:
+      break
   if chute in sorteio:
+    print(forca.get(c1-1))
     print('\n\033[32mAcertou\033[m\n')
     letras_certas.append(chute)
     for letra in sorteio:
       if letra in letras_certas:
         print('\033[33m',letra,'\033[m' , end='')
+        c2 += 1
       else:
         print(' _ ', end='')
     print()
-    # Faltou descobrir como fazer a contagem das letras já descobertas e igualar elas com o tamanho da palavra. Problema: letras repetidas.
-    if len(letras_certas) == len(sorteio):
-      print(f'\033[32mParabéns!! Você ganhou!\033[m')
+    if c2 == len(sorteio):
+      print(f'\033[32m\nParabéns!! Você ganhou!\033[m\n')
       break
+    else:
+      c2 = 0
   else: 
     print(f'\n\033[31mLetra {chute} não está na palavra.\033[m')
     letras_erradas.append(chute)
@@ -94,8 +102,8 @@ while True:
         print(' _ ', end='')
     print()
   if c1 == 8:
-    print(f'\033[31mVocê perdeu.\033[m\nA palavra era \033[32m{sorteio}\033[m.')
-    print('Mais sorte na próxima vez.')
+    print(f'\n\033[31mVocê perdeu.\033[m\nA palavra era \033[32m{sorteio}\033[m.')
+    print('Mais sorte na próxima vez.\n')
     break
   print('\nLetras corretas: ', end='')
   print(*letras_certas)
---
+++ palavras.py
@@ -0,0 +1,6 @@
+import random
+
+def palavras():
+    todas = ['palavra', 'luminaria', 'quadro', 'computador', 'metastase', 'planejamento', 'semana', 'acender', 'afilhado', 'ardiloso', 'aspero', 'assombraçao', 'basquete',
+    'chiclete', 'esquerdo', 'contexto', 'coelho', 'impacto', 'modernidade', 'paralelepipedo', 'quarentena', 'covid', 'taciturno', 'assombrado', 'fugaz', 'eloquente']
+    return random.choice(todas)
